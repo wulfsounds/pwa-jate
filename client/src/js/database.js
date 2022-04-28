eddba369f342b69e7021de653ca7c8e8b2a2028f
Devin Wulf <devinlwulf@gmail.com>
2022-04-28 13:25:32 -0500

    update modified file structure

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,42 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+/*
+  We need to add some code below which will take updated content and save it to IndexedDB.
+*/
+export const putDb = async (content) => {
+  // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
+  const DB_VAR = await openDB('DB_NAME', 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Now create a variable for the transaction
+  const TX_VAR = jateDb.transaction('DB_NAME', 'readwrite');
+
+  // Now create a variable for the store
+  const STORE_VAR = tx.objectStore('DB_NAME');
+
+  // Now create a variable named "request" and have it perform the update
+  const VAR_NAME = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
+};
+
+/*
+  We need to add some code below which will get all content from IndexedDB.
+*/
+export const getDb = async () => {
+  // You can duplicate the same first lines of code from above, except that the transaction will be 'readonly'
+  
+  // LINES 1-3 HERE
+
+  // Leave the rest as-is
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  return result?.value;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,32 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+/*
+  Here we need to provide the logic for when and how to install the PWA. 
+*/
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+//  Keep this code as-is
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// This listener fires when the user clicks the "install" button.
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+  // TODO: Notice line 9 above. We want the same line here, but the value should be set to null now.
+  // Insert line here
+
+  // TODO: Notice line 10 above. We want the same line here, but the hidden value should be set to true.
+  // Insert line here
+});
+
+window.addEventListener('appinstalled', (event) => {
+  window.deferredPrompt = null;
+});
