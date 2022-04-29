ab00deafa204f522ab54d960be528924267aaa68
Devin Wulf <devinlwulf@gmail.com>
2022-04-29 13:51:34 -0500

    update heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,18 +13,25 @@ const initdb = async () =>
   });
 
 export const putDb = async (content) => {
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  const textDB = await openDB('textDB', 1);
+
+  const trans = jateDb.transaction('textDB', 'readwrite');
+
+  const stre = tx.objectStore('textDB');
+
   const request = store.put({ id: 1, value: content });
+
   const result = await request;
   console.log('🚀 - data saved to the database', result.value);
 };
 
 export const getDb = async () => {
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
+  const textDB = await openDB('textDB', 1);
+
+  const trans = jateDb.transaction('textDB', 'readonly');
+
+  const stre = tx.objectStore('textDB');
+
   const request = store.get(1);
   const result = await request;
   result
